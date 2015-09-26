baa032b8f3c5635e631a141302b0daa9d125f004
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 17:42:10 +0100

    add application launcher w/ empty dialog

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+'use strict'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import UserSignupDialog from './membership/user-signup/dialog'
+
+ReactDOM.render(<UserSignupDialog />, document.getElementById('react'))
