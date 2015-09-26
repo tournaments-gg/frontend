cde627470297dcbf4b99a02d52250e2b1863ee44
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 19:11:11 +0100

    add sign up form fields

--- src/membership/user-signup/dialog.jsx
+++ src/membership/user-signup/dialog.jsx
@@ -7,8 +7,27 @@ export default React.createClass({
   },
 
   render () {
-    return <div>
-      {this.props.strategies.map((strategy) => <button key={strategy.id}>{strategy.name}</button>)}
-    </div>
+    return <section>
+      <h1>Sign Up</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" />
+
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" name="email" />
+
+        <label htmlFor="termsAndConditions">I agree to the terms and conditions</label>
+        <input type="checkbox" name="termsAndConditions" />
+      </form>
+
+      <div>
+        <p>Alternatively, you can sign up using one of the following providers:</p>
+        {this.props.strategies.map((strategy) => <button key={strategy.id}>{strategy.name}</button>)}
+      </div>
+    </section>
   }
 })
