76a3afeedfdd60c0c143a9cedfea81c5f9ab2ad1
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 18:06:55 +0100

    allow props to be shallowly rendered

--- src/test-utils/shallow-render.jsx
+++ src/test-utils/shallow-render.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 // Don't really need to create renderers per instance (right?)
 const renderer = TestUtils.createRenderer()
-export default function shallowRender(componentClass) {
-  const element = React.createElement(componentClass)
+export default function shallowRender(componentClass, props) {
+  const element = React.createElement(componentClass, props)
   renderer.render(element)
   return renderer.getRenderOutput()
 }
