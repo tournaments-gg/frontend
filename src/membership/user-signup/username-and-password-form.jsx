60aa6adaeb1236fd664ed7d53f989f11be1ca932
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 22:40:32 +0100

    disable submit when the form is invalid

--- src/membership/user-signup/username-and-password-form.jsx
+++ src/membership/user-signup/username-and-password-form.jsx
@@ -5,15 +5,29 @@ import Checkbox from './checkbox'
 import {Form} from 'formsy-react'
 
 export default React.createClass({
+  getInitialState () {
+    return {
+      submitDisabled: true
+    }
+  },
+
   render () {
     // TODO: Add a link to the T&C?
-    return <Form>
+    return <Form onSubmit={this.submit} onValid={this.enableSubmit} onInvalid={this.disableSubmit} >
       <Field type='text' name='username' label='Username' />
       <Field type='password' name='password' label='Password' />
       <Field type='password' name='confirmPassword' label='Confirm Password' />
       <Field type='email' name='email' label='E-Mail' />
       <Checkbox name='tAndC' text='I agree to the terms and conditions.' />
-      <input type="submit" value="Sign Up" />
+      <input type="submit" value="Sign Up" disabled={this.state.submitDisabled} />
     </Form>
+  },
+
+  enableSubmit () {
+    this.setState({ submitDisabled: false })
+  },
+
+  disableSubmit () {
+    this.setState({ submitDisabled: true })
   }
 })
