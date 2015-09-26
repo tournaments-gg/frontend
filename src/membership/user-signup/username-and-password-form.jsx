8974c1556b6b7f25878ce7fddeaa5bd0b5a20a18
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 19:34:59 +0100

    add fields to form groups

--- src/membership/user-signup/username-and-password-form.jsx
+++ src/membership/user-signup/username-and-password-form.jsx
@@ -4,17 +4,25 @@ import React from 'react'
 export default React.createClass({
   render () {
     return <form>
-      <label htmlFor='username'>Username</label>
-      <input type='text' name='username' />
+      <div>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' />
+      </div>
 
-      <label htmlFor='password'>Password</label>
-      <input type='password' name='password' />
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' />
+      </div>
 
-      <label htmlFor='email'>E-Mail</label>
-      <input type='email' name='email' />
+      <div>
+        <label htmlFor='email'>E-Mail</label>
+        <input type='email' name='email' />
+      </div>
 
-      <label htmlFor='termsAndConditions'>I agree to the terms and conditions</label>
-      <input type='checkbox' name='termsAndConditions' />
+      <div>
+        <label htmlFor='termsAndConditions'>I agree to the terms and conditions</label>
+        <input type='checkbox' name='termsAndConditions' />
+      </div>
     </form>
   }
 })
