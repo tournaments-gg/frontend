02672ffc28396b517a6adab992a4abe838724f6e
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 20:03:43 +0100

    remove unnecessary {}

--- src/membership/user-signup/username-and-password-form.jsx
+++ src/membership/user-signup/username-and-password-form.jsx
@@ -6,17 +6,17 @@ export default React.createClass({
   render () {
     return <form>
       <div>
-        <Label for='username' text={'Username'} screenReaderOnly={true} />
+        <Label for='username' text='Username' screenReaderOnly />
         <input type='text' name='username' placeholder='Username' />
       </div>
 
       <div>
-        <Label for='password' text={'Password'} screenReaderOnly={true} />
+        <Label for='password' text='Password' screenReaderOnly />
         <input type='password' name='password' placeholder='Password' />
       </div>
 
       <div>
-        <Label for='email' text={'E-Mail'} screenReaderOnly={true} />
+        <Label for='email' text='E-Mail' screenReaderOnly />
         <input type='email' name='email' placeholder='foo@example.com' />
       </div>
 
