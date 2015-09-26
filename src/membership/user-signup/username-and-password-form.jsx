3528542d485e4e23510ad98f279dd2c1cc2c1c2b
Dan Pantry <dan@lambdaexpression.io>
2015-09-26 22:34:42 +0100

    add confirm password field; move email below passwords

--- src/membership/user-signup/username-and-password-form.jsx
+++ src/membership/user-signup/username-and-password-form.jsx
@@ -9,8 +9,9 @@ export default React.createClass({
     // TODO: Add a link to the T&C?
     return <Form>
       <Field type='text' name='username' label='Username' />
-      <Field type='email' name='email' label='E-Mail' />
       <Field type='password' name='password' label='Password' />
+      <Field type='password' name='confirmPassword' label='Confirm Password' />
+      <Field type='email' name='email' label='E-Mail' />
       <Checkbox name='tAndC' text='I agree to the terms and conditions.' />
       <input type="submit" value="Sign Up" />
     </Form>
