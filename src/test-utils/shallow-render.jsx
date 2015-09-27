038a1cf9eda72cb214fce9a14b9fdbdce0800a9c
Dan Pantry <dan@lambdaexpression.io>
2015-09-27 17:06:46 +0100

    create a renderer per instance
    woops.

--- src/test-utils/shallow-render.jsx
+++ src/test-utils/shallow-render.jsx
@@ -2,9 +2,8 @@
 import TestUtils from 'react-addons-test-utils'
 import React from 'react'
 
-// Don't really need to create renderers per instance (right?)
-const renderer = TestUtils.createRenderer()
 export default function shallowRender(componentClass, props) {
+  const renderer = TestUtils.createRenderer()
   const element = React.createElement(componentClass, props)
   renderer.render(element)
   return renderer.getRenderOutput()
